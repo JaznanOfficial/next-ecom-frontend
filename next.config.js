7df88fac29f65263e127ce218e6b43a00e6f70db
JaznanOfficial <jaznanofficial@gmail.com>
2023-04-10 13:25:10 +0600

    some update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     env: {
         API_URL: "https://next-ecom-server.cyclic.app/api/v1",
         DB_URI: "mongodb+srv://next-shop:XFIyT3HuU2XR9PRt@next-blog.jg4cbvm.mongodb.net/?retryWrites=true&w=majority",
